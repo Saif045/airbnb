11ff83bce3e4950052817239bdfe437753120a95
Saif045 <saif.x45@gmail.com>
2023-04-23 10:21:23 +0200

    prisma type fix

--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -3,6 +3,9 @@ import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
+
+
+
 export async function POST(
   request: Request, 
 ) {
