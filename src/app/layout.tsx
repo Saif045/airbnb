abaf1d09ee8ed4d79d5f091d92f5d045bb506cae
Saif045 <saif.x45@gmail.com>
2023-05-02 08:11:33 +0300

    modal style fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,27 @@
-import { Nunito } from 'next/font/google'
-
-import Navbar from '@/src/components/navbar/Navbar';
-import LoginModal from '@/src/components/modals/LoginModal';
-import RegisterModal from '@/src/components/modals/RegisterModal';
-import SearchModal from '@/src/components/modals/SearchModal';
-import RentModal from '@/src/components/modals/RentModal';
-
-import ToasterProvider from '@/src/providers/ToasterProvider';
-
-import './globals.css'
-import ClientOnly from '@/src/components/ClientOnly';
-import getCurrentUser from '@/src/actions/getCurrentUser';
+import { Nunito } from "next/font/google";
+import Navbar from "@/src/components/navbar/Navbar";
+import LoginModal from "@/src/components/modals/LoginModal";
+import RegisterModal from "@/src/components/modals/RegisterModal";
+import SearchModal from "@/src/components/modals/SearchModal";
+import RentModal from "@/src/components/modals/RentModal";
+import ToasterProvider from "@/src/providers/ToasterProvider";
+import ClientOnly from "@/src/components/ClientOnly";
+import getCurrentUser from "@/src/actions/getCurrentUser";
+import "./globals.css";
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
-}
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
 
-const font = Nunito({ 
-  subsets: ['latin'], 
+const font = Nunito({
+  subsets: ["latin"],
 });
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   const currentUser = await getCurrentUser();
 
@@ -39,10 +36,8 @@ export default async function RootLayout({
           <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        <div className="pb-20 pt-28">
-          {children}
-        </div>
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
-  )
+  );
 }
--- src/components/modals/RentModal.tsx
+++ src/components/modals/RentModal.tsx
@@ -140,7 +140,8 @@ const RentModal = () => {
           grid-cols-1 
           md:grid-cols-2 
           gap-3
-          max-h-[50vh]
+          max-h-[40vh]
+         
           overflow-y-auto
         "
       >
